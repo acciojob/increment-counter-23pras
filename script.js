e2999b4e9cc6cd673268996ecc00ad71fb7e0b2b
Prashant Pal <palprashant465@gmail.com>
2025-03-14 21:57:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+// Initial counter value
+let count = 0;
+
+// Function to increment the counter
+function incrementCounter() {
+  count++; // Increase the count by 1
+  document.getElementById('counter').innerText = count; // Update the DOM
+}
